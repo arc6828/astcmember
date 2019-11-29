9afdfc09e77c7c5466a6ca514f46641417f478b8
arc6828 <chavalit.kow@gmail.com>
2019-11-29 09:52:22 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,9 +63,9 @@
                                 <tr><th> สถาบัน </th><td> {{ $profile->school }} </td></tr>
                                 <tr><th> คณะ/ภาควิชา </th><td> {{ $profile->major }} </td></tr>
                                 <tr><th> เลขที่ ซอย ถนน ตำบล </th><td> {{ $profile->address }} </td></tr>
-                                <tr><th> ตำบล </th><td> {{ $profile->district_district->district }} </td></tr>
-                                <tr><th> อำเภอ </th><td> {{ $profile->district_amphoe->amphoe }} </td></tr>
-                                <tr><th> จังหวัด </th><td> {{ $profile->district_province->province }} </td></tr>
+                                <tr><th> ตำบล </th><td> {{ $profile->district? $profile->district_district->district : '' }} </td></tr>
+                                <tr><th> อำเภอ </th><td> {{ $profile->amphoe? $profile->district_amphoe->amphoe : '' }} </td></tr>
+                                <tr><th> จังหวัด </th><td> {{ $profile->province? $profile->district_province->province : '' }} </td></tr>
                                 <tr><th> รหัสไปรษณีย์ </th><td> {{ $profile->postnumber }} </td></tr>
                                 <tr><th> เบอร์โทรศัพท์ </th><td> {{ $profile->tel }} </td></tr>
                                 <tr><th> เบอร์โทรสาร </th><td> {{ $profile->fax }} </td></tr>
@@ -73,9 +73,9 @@
                                 <tr><th> สถาบัน </th><td> {{ $profile->bill_school }} </td></tr>
                                 <tr><th> คณะ/ภาควิชา </th><td> {{ $profile->bill_major }} </td></tr>
                                 <tr><th> เลขที่ ซอย ถนน ตำบล </th><td> {{ $profile->bill_address }} </td></tr>
-                                <tr><th> ตำบล </th><td> {{ $profile->district_bill_district->district }} </td></tr>
-                                <tr><th> อำเภอ </th><td> {{ $profile->district_bill_amphoe->amphoe }} </td></tr>
-                                <tr><th> จังหวัด </th><td> {{ $profile->district_bill_province->province }} </td></tr>
+                                <tr><th> ตำบล </th><td> {{ $profile->bill_district? $profile->district_bill_district->district : ''  }} </td></tr>
+                                <tr><th> อำเภอ </th><td> {{ $profile->bill_amphoe? $profile->district_bill_amphoe->amphoe : '' }} </td></tr>
+                                <tr><th> จังหวัด </th><td> {{ $profile->bill_province? $profile->district_bill_province->province : '' }} </td></tr>
                                 <tr><th> รหัสไปรษณีย์ </th><td> {{ $profile->bill_postnumber }} </td></tr>
                                 <tr><th> เบอร์โทรศัพท์ </th><td> {{ $profile->bill_tel }} </td></tr>
                                 <tr><th> เบอร์โทรสาร </th><td> {{ $profile->bill_fax }} </td></tr>
