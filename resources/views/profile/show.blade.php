63d1b406134dfbb70a0acde6ede8add937340d9f
arc6828 <chavalit.kow@gmail.com>
2020-01-15 13:58:42 +0700

    separate profile/show-item

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -21,42 +21,7 @@
                         <br/>
                         <br/>
 
-                        <div class="table-responsive">
-                            <table class="table">
-                                <tbody>
-                                    <tr>
-                                        <th>ID</th><td>{{ $profile->id }}</td>
-                                    </tr>
-                                    <tr><th> เพศ </th><td> {{ $profile->sex }} </td></tr>
-                                    <tr><th> คำนำหน้าชื่อ </th><td> {{ $profile->title }} </td></tr>
-                                    <tr><th> ชื่อ </th><td> {{ $profile->name }} </td></tr>
-                                    <tr><th> นามสกุล </th><td> {{ $profile->lastname }} </td></tr>
-                                    <tr><th> อีเมล </th><td> {{ $profile->email }} </td></tr>
-                                    <tr><th> สถานภาพ </th><td> {{ $profile->status }} </td></tr>
-                                    <tr><th> สถานภาพอื่นๆ โปรดระบุ </th><td> {{ $profile->statusothers }} </td></tr>
-                                    <tr><th> อาหารกลางวัน </th><td> {{ $profile->food }} </td></tr>
-                                    <tr><th> สถาบัน </th><td> {{ $profile->school }} </td></tr>
-                                    <tr><th> คณะ/ภาควิชา </th><td> {{ $profile->major }} </td></tr>
-                                    <tr><th> เลขที่ ซอย ถนน </th><td> {{ $profile->address }} </td></tr>
-                                    <tr><th> อำเภอ </th><td> {{ $profile->amphoe }} </td></tr>
-                                    <tr><th> ตำบล </th><td> {{ $profile->district }} </td></tr>
-                                    <tr><th> จังหวัด </th><td> {{ $profile->province }} </td></tr>
-                                    <tr><th> รหัสไปรษณีย์ </th><td> {{ $profile->postnumber }} </td></tr>
-                                    <tr><th> เบอร์โทรศัพท์ </th><td> {{ $profile->tel }} </td></tr>
-                                    <tr><th> เบอร์โทรสาร </th><td> {{ $profile->fax }} </td></tr>
-                                    <tr><th> ไฟล์หลักฐานการชำระเงินค่าลงทะเบียน </th><td> {{ $profile->fileregister }} </td></tr>
-                                    <tr><th> สถาบัน </th><td> {{ $profile->bill_school }} </td></tr>
-                                    <tr><th> คณะ/ภาควิชา </th><td> {{ $profile->bill_major }} </td></tr>
-                                    <tr><th> เลขที่ ซอย ถนน </th><td> {{ $profile->bill_address }} </td></tr>
-                                    <tr><th> อำเภอ </th><td> {{ $profile->bill_amphoe }} </td></tr>
-                                    <tr><th> ตำบล </th><td> {{ $profile->bill_district}} </td></tr>
-                                    <tr><th> จังหวัด </th><td> {{ $profile->bill_province }} </td></tr>
-                                    <tr><th> รหัสไปรษณีย์ </th><td> {{ $profile->bill_postnumber }} </td></tr>
-                                    <tr><th> เบอร์โทรศัพท์ </th><td> {{ $profile->bill_tel }} </td></tr>
-                                    <tr><th> เบอร์โทรสาร </th><td> {{ $profile->bill_fax }} </td></tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        @include('profile/show-item')
 
                     </div>
                 </div>
