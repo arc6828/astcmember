adbf32cc632b338babca90611e9a889443aecc5c
ebenze31 <52275898+ebenze31@users.noreply.github.com>
2020-02-23 22:54:39 +0700

    mail accept

--- resources/views/accept/acceptmail.blade.php
+++ resources/views/accept/acceptmail.blade.php
@@ -1,4 +1,2 @@
 <p>กรุณาประเมินบทความ โดยคลิกที่ลิงค์ด้านล่าง</p><br>
-@foreach($articleevaluation as $item)
-<p> {{ url('/article-evaluation/create?article_id=' . $item->id) }} </p> 
-@endforeach
+<p> {{ url('/article-evaluation/create?article_id=' . $accept->id) }} </p> 
