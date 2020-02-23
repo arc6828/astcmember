ea8c794acb4a61336e68f2e1b413cff42e941b87
ebenze31 <52275898+ebenze31@users.noreply.github.com>
2020-02-23 22:56:44 +0700

    Update acceptmail.blade.php

--- resources/views/accept/acceptmail.blade.php
+++ resources/views/accept/acceptmail.blade.php
@@ -1,2 +1,2 @@
 <p>กรุณาประเมินบทความ โดยคลิกที่ลิงค์ด้านล่าง</p><br>
-<p> {{ url('/article-evaluation/create?article_id=' . $accept->id) }} </p> 
+<p> {{ url('/article-evaluation/create?article_id=' . $accept->article_id) }} </p> 
