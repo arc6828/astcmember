8141a41d52960dc873aa795ce81b9eeaae134c01
ebenze31 <52275898+ebenze31@users.noreply.github.com>
2020-02-23 22:28:28 +0700

    Update acceptmail.blade.php

--- resources/views/accept/acceptmail.blade.php
+++ resources/views/accept/acceptmail.blade.php
@@ -1,1 +1,4 @@
-<p>กรุณาประเมินบทความ โดยคลิกที่ลิงค์ด้านล่าง</p><br> 
+<p>กรุณาประเมินบทความ โดยคลิกที่ลิงค์ด้านล่าง</p><br>
+@foreach($articleevaluation as $item)
+<p> {{ url('/article-evaluation/create?article_id=' . $item->id) }} </p> 
+@endforeach
