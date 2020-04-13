3d38eeb19649f644e89503f2eb56b3b071ca7ab4
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-04-13 12:26:40 +0700

    Modal Postpone

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,4 +70,33 @@
         </div>
     </div>
 </div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary d-none" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <div>
+                    <img src="{{ url('/img/banner-11-3-63-3-resize.jpg') }}" width="100%">
+                </div>
+            </div>
+        
+        </div>
+    </div>
+</div>
+<script>
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#exampleModal').modal('show');
+});
+</script>
 @endsection
